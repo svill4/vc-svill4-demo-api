ef113eb0a43dfaffe8d424f6273f7307318b7d74
Santiago Villarreal <santiago.villarreal@monsanto.com>
2018-05-15 11:03:08 -0300

    Initial commit

---
+++ src/main/java/com/monsanto/finance/vc/VcSvill4DemoApiApplication.java
@@ -0,0 +1,12 @@
+package com.monsanto.finance.vc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcSvill4DemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcSvill4DemoApiApplication.class, args);
+	}
+}
